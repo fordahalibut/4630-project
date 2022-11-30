06850cd6a65cab49d8996f6c59c9f2701cf42b20
RICHARD GILLESPIE <u1328400@umail.utah.edu>
2022-11-30 11:05:24 -0700

    adding logic for selectable axes

--- js/scatter.js
+++ js/scatter.js
@@ -14,6 +14,35 @@ class ScatterPlot{
 
         this.vizWidth = 600;
         this.vizHeight = 500;
+
+        this.numericIndicators = [{
+            indicator: "hit_points",
+            label: "HP"
+        },
+        {
+            indicator: "armor_class",
+            label: "Armor Class"
+        },
+        {
+            indicator: "challenge_rating",
+            label: "Challenge Rating"
+        },    {
+            indicator: "xp",
+            label: "XP Value"
+        }];
+
+        this.categoryIndicators = [{
+            indicator: "type",
+            label: "Type"
+        },
+        {
+            indicator: "alignment",
+            label: "Alignment"
+        },
+        {
+            indicator: "size",
+            label: "Size"
+        }];
     }
 
     /**TODO: 
@@ -26,6 +55,48 @@ class ScatterPlot{
 
     drawScatterplot(){
 
+        // Append tooltip div
+        let chartArea = d3.select('#scatter-area')
+
+        let tooltip = chartArea
+            .append('div')
+            .classed('tooltip', true)
+            .style('opacity', 0);
+
+        // Append chart svg
+        let svg = chartArea.append('svg')
+            .attr('width', this.vizWidth + this.margin.left + this.margin.right)
+            .attr('height', this.vizHeight + this.margin.top + this.margin.bottom);
+        
+        let chartGroup = svg.append('g').classed('wrapper-group', true);
+
+        // Append axes
+        let x = chartGroup.append('g')
+            .classed('axis', true)
+            .attr('id', 'x-axis')
+            .attr('transform', `translate(0, ${this.vizHeight})`)
+            .append('text').text('')
+            .classed('axis-label', true)
+            .attr('transform', `translate(${this.vizWidth / 2}, 35)`)
+            .attr('text-align', 'center');
+
+        let y = chartGroup.append('g')
+            .classed('axis', true)
+            .attr('id', 'y-axis')
+            .attr("transform", `translate(${this.margin.bottom * 2}, 0)`)
+            .append("text").text('')
+            .classed("axis-label", true)
+            .attr("transform", `translate(-35, ${this.vizHeight/2}) rotate(270)`)
+            .attr("text-align", "center");
+
+        // Append dropdowns
+        let dropdown = chartArea.append('div')
+            .classed('dropdown-wrapper', true);
+
+
+
+
+
         this.xScale = d3.scaleLinear()
             .domain([0, d3.max(this.data, d=> d.challenge_rating)])
             .range([this.margin.left + this.margin.right, this.vizWidth]);
@@ -34,17 +105,18 @@ class ScatterPlot{
             .domain([0, d3.max(this.data, d=> d.hit_points)])
             .range([this.vizHeight, this.margin.top + this.margin.bottom]);
         
-        let chartArea = d3.select('#scatter-area')
-            .append('svg')
-            .attr('width', this.vizWidth + this.margin.left + this.margin.right)
-            .attr('height', this.vizHeight + this.margin.top + this.margin.bottom);
 
-        chartArea.selectAll('circle')
+
+        svg.selectAll('circle')
             .data(this.data)
             .join('circle')
             .attr('cx', d=> this.xScale(d.challenge_rating))
             .attr('cy', d=> this.yScale(d.hit_points))
             .attr('r', 5);      
+
+    }
+
+    updateScatterplot(xAxis, yAxis, colorAxis){
 
     }
 }
