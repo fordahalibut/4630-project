f6ce091969f9398a6637a300a9c372983c5611ac
RICHARD GILLESPIE <u1328400@umail.utah.edu>
2022-12-03 18:42:15 -0700

    table statistics

--- js/table.js
+++ js/table.js
@@ -36,41 +36,82 @@ class Table {
                 key: 'armor_class',
                 sorted: false,
                 ascending: false 
+            },
+            {
+                key: 'xp',
+                sorted: false,
+                ascending: false 
             }
         ]
 
-        this.columnNames = ['Name', 'Type', 'Challenge Rating', 'Hit Points', 'Armor Class', 'Stats'];
+        this.columnNames = ['Name', 'Type', 'Challenge Rating', 'Hit Points', 'Armor Class', 'XP Value', 'Stats'];
 
         this.table = d3.select('#dataTable');
         this.rows = this.table.select('tbody').selectAll('tr');
 
-       // Define scales for svg elements
+        // Define scales for svg elements
+        this.statScale = d3.scaleLinear()
+            .domain([0, 30])
+            .range([this.vizHeight, 0]);
+        
+        this.xScale = d3.scaleLinear()
+            .domain([0,6]).nice()
+            .range([20, this.vizWidth - 20]);
+
+        this.colorScale = d3.scaleOrdinal(d3.schemeSet3);
 
         this.attachSortHandlers();
-        //this.drawLegends();
+        this.drawLegends();
         this.updateHeaders();
     }
 
     // Draw legends for the svg elements
     drawLegends() {
-        // TODO: decide visual representation for stats
+
+        let legendLabels = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'];
+        let legendData = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5];
+
+        let xAxis = d3.axisBottom()
+            .scale(this.xScale)
+            .ticks(legendData.length)
+            .tickFormat((d,i) => legendLabels[i]);
+
+        d3.select('#statViz')
+            .attr('width', this.vizWidth)
+            .attr('height', this.vizHeight)
+            .call(xAxis);
+
+        d3.select('#columnHeaders').selectAll('line').remove();
+        d3.select('#columnHeaders').selectAll('path').remove();
 
     }
 
     drawTable() {
+
+        d3.select('#dataTableBody').selectAll('rect').remove();
         this.updateHeaders();
 
-        let rowSelection = d3.select('#dataTableBody')
+        // Append tooltip div
+        this.tooltip = d3.select('#dataTable')
+            .append('div')
+            .classed('tooltip', true)
+            .attr('id', 'table-tooltip')
+            .style('opacity', 0);
+
+        let rows = d3.select('#dataTableBody')
             .selectAll('tr')
             .data(this.filteredData)
-            .join('tr');
+            .join('tr')
+            .attr('id', d => `row-${d.index}`);
 
-        let selection = rowSelection.selectAll('td')
+        let tds = rows.selectAll('td')
             .data(this.rowToCellDataTransform)
             .join('td');
 
-        selection.filter(d => d.type === 'text')
+        tds.filter(d => d.type === 'text')
             .text(d => d.value);
+
+        this.drawRects(tds);            
     }
 
     rowToCellDataTransform(d) {
@@ -99,7 +140,22 @@ class Table {
             value: d.armor_class
         };
 
-        let dataList = [nameInfo, typeInfo, crInfo, hpInfo, acInfo];
+        let xpInfo = {
+            type: 'text',
+            value: d.xp
+        };
+
+        let statInfo = {
+            type: 'viz',
+            str: d.strength,
+            dex: d.dexterity,
+            con: d.constitution,
+            int: d.intelligence,
+            wis: d.wisdom,
+            cha: d.charisma
+        };
+
+        let dataList = [nameInfo, typeInfo, crInfo, hpInfo, acInfo, xpInfo, statInfo];
         return dataList;
     }
 
@@ -115,6 +171,53 @@ class Table {
                     .classed('sorting', true);
             }
         }
+    }
+
+    drawRects(tds) {
+
+        let statArr = ['str', 'dex', 'con', 'int', 'wis', 'cha'];
+        let labelArr = ['Strength: ', 'Dexterity: ', 'Constitution: ', 'Intelligence: ', 'Wisdom: ', 'Charisma: ']
+        
+        let stats = tds.filter(d => d.type === 'viz')
+            .classed('text-center', true)
+            .selectAll('svg')
+            .data(d => [d])
+            .join('svg')
+            .attr('width', this.vizWidth)
+            .attr('height', this.vizHeight)
+            .classed('stats', true);
+        
+        statArr.forEach((e, i) => {
+
+            stats.join('rect')
+                .append('rect')
+                .attr('x', d => this.xScale(i))
+                .attr('y', d => this.statScale(d[e]))
+                .attr('width', 20)
+                .attr('height', d => 30 - this.statScale(d[e]))
+                .on('mouseover', function(d) {
+
+                    let pos = d3.select(this).node().getBoundingClientRect();
+    
+                    d3.select(this)
+                        .style('stroke-width', 2);
+    
+                    let content = labelArr[i] + d.path[0].__data__[e];
+                    
+                    d3.select('#table-tooltip')
+                        .style('opacity', 0.9)
+                        .html(`<h4>${content}</h4`)
+                        .style('left', `${pos['x'] + 5}px`)
+                        .style('top', `${(window.pageYOffset  + pos['y'] - 15)}px`);
+                })
+                .on('mouseout', function(d) {
+                    d3.select(this).style('stroke-width', 1);
+                    d3.select('.tooltip')
+                    .attr('transform', 'translate(0,0)')
+                    .style('opacity', 0);
+                });
+        });
+
     }
 
     attachSortHandlers()
@@ -135,6 +238,7 @@ class Table {
                     this.headerData[2].sorted = false;
                     this.headerData[3].sorted = false;
                     this.headerData[4].sorted = false;
+                    this.headerData[5].sorted = false;
                     this.headerData[0].ascending = !this.headerData[0].ascending;
 
                     if (this.headerData[0].ascending) {
@@ -151,6 +255,7 @@ class Table {
                     this.headerData[2].sorted = false;
                     this.headerData[3].sorted = false;
                     this.headerData[4].sorted = false;
+                    this.headerData[5].sorted = false;
                     this.headerData[1].ascending = !this.headerData[1].ascending;
 
                     if (this.headerData[1].ascending) {
@@ -167,6 +272,7 @@ class Table {
                     this.headerData[2].sorted = true;
                     this.headerData[3].sorted = false;
                     this.headerData[4].sorted = false;
+                    this.headerData[5].sorted = false;
                     this.headerData[2].ascending = !this.headerData[2].ascending;
 
                     if (this.headerData[2].ascending) {
@@ -183,6 +289,7 @@ class Table {
                     this.headerData[2].sorted = false;
                     this.headerData[3].sorted = true;
                     this.headerData[4].sorted = false;
+                    this.headerData[5].sorted = false;
                     this.headerData[3].ascending = !this.headerData[3].ascending;
 
                     if (this.headerData[3].ascending) {
@@ -199,6 +306,7 @@ class Table {
                     this.headerData[2].sorted = false;
                     this.headerData[3].sorted = false;
                     this.headerData[4].sorted = true;
+                    this.headerData[5].sorted = false;
                     this.headerData[3].ascending = !this.headerData[3].ascending;
 
                     if (this.headerData[3].ascending) {
@@ -208,6 +316,24 @@ class Table {
                         this.filteredData.sort((a,b) => d3.descending(+a.armor_class, +b.armor_class));
                     }
                 }
+
+                else if (choice === 'xp') {
+                    this.headerData[0].sorted = false;
+                    this.headerData[1].sorted = false;
+                    this.headerData[2].sorted = false;
+                    this.headerData[3].sorted = false;
+                    this.headerData[4].sorted = false;
+                    this.headerData[5].sorted = true;
+                    this.headerData[3].ascending = !this.headerData[3].ascending;
+
+                    if (this.headerData[3].ascending) {
+                        this.filteredData.sort((a,b) => d3.ascending(+a.xp, +b.xp));
+                    }
+                    else {
+                        this.filteredData.sort((a,b) => d3.descending(+a.xp, +b.xp));
+                    }
+                }
+
                 this.drawTable();
             })
 
